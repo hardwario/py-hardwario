88b6a1c1a9acb20c75446fb15d03377b983e325d
Karel Blavka <karel.blavka@hardwario.com>
2025-01-24 10:33:30 +0100

    add chester module
    Signed-off-by: Karel Blavka <karel.blavka@hardwario.com>

---
+++ hardwario/chester/utils.py
@@ -0,0 +1,24 @@
+from os.path import join, exists, isfile, getsize, expanduser
+
+
+DEFAULT_CACHE_PATH = expanduser("~/.hardwario/chester/cache")
+
+
+def test_file(*paths):
+    file_path = join(*paths)
+    if exists(file_path) and isfile(file_path) and getsize(file_path) > 0:
+        return file_path
+
+
+def find_hex(app_path, no_exception=False):
+    out_path = join(app_path, 'build', 'zephyr')
+
+    for name in ('merged.hex', 'zephyr.hex'):
+        hex_path = test_file(out_path, name)
+        if hex_path:
+            return hex_path
+
+    if no_exception:
+        return None
+
+    raise Exception('No firmware found.')
