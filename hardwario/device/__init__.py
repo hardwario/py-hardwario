f34590299f7458440fbee055046067fe7dc85c28
Karel Blavka <karel.blavka@hardwario.com>
2025-09-05 08:18:12 +0200

    Add detection of J-Link probe firmware mismatch with prompt for update/downgrade
    Signed-off-by: Karel Blavka <karel.blavka@hardwario.com>

--- hardwario/device/__init__.py
+++ hardwario/device/__init__.py
@@ -1,5 +1,6 @@
 from loguru import logger
 import pylink
+import click
 
 
 def jlink_setup(device, serial_no=None, speed=2000):
@@ -17,8 +18,33 @@ def jlink_setup(device, serial_no=None, speed=2000):
         pass
     logger.info(f'J-Link serial_number: {jlink.serial_number}')
     logger.info(f'J-Link firmware_version: {jlink.firmware_version}')
-    logger.info(f'J-Link firmware_outdated: {jlink.firmware_outdated()}')
-    logger.info(f'J-Link firmware_newer: {jlink.firmware_newer()}')
+
+    try:
+        firmware_outdated = jlink.firmware_outdated()
+        logger.info(f'J-Link firmware_outdated: {firmware_outdated}')
+    except Exception as _:
+        firmware_outdated = False
+        logger.info('J-Link firmware_outdated: not supported')
+
+    try:
+        firmware_newer = jlink.firmware_newer()
+        logger.info(f'J-Link firmware_newer: {firmware_newer}')
+    except Exception as _:
+        firmware_newer = False
+        logger.info('J-Link firmware_newer: not supported')
+
+    if firmware_outdated:
+        if click.confirm('Newer firmware version available. Can you update?', default=False):
+            jlink.update_firmware()
+            logger.info('Firmware updated. Please restart the program.')
+            exit(0)
+
+    if firmware_newer:
+        if click.confirm('Firmware version is newer than DLL supports. Synchonize firmware with DLL?', default=False):
+            jlink.invalidate_firmware()
+            jlink.update_firmware()
+            logger.info('Firmware synchronized. Please restart the program.')
+            exit(0)
 
     logger.info(f'J-Link device: {device}')
 
